84cad172378b1b3d9cea8802793d481c0daaa577
SuwillWD <suwilllokhande688@gmail.com>
2024-06-02 22:37:35 +0530

    Implement class refactor

--- index.js
+++ index.js
@@ -10,112 +10,120 @@ const deleteCard = document.querySelectorAll("delete-card");
 const editCard = document.querySelectorAll("edit-card");
 
 
-//myLibrary array for storing books data
-const myLibrary = [];
+class library {
 
-// constructor function for creating new book object
-function Book(title, author, noOfPages, readStatus) {
-    this.title = title;
-    this.author = author;
-    this.noOfPages = noOfPages;
-    this.readStatus = readStatus;
-}
+    //myLibrary array for storing books data
+    static myLibrary = [];
 
-// function to add new book object into myLibrary array
-function addBookToLibrary(title, author, noOfPages, readStatus) {
-    const book = new Book(title, author, noOfPages, readStatus);
-    myLibrary.push(book);
-}
+    // function to add new book object into myLibrary array
+    addBookToLibrary(book) {
+        library.myLibrary.push(book);
+    };
 
-function createCard(book) {
-    const bookCard = document.createElement("div");
-    bookCard.setAttribute("class", "book-card");
-    const titleDiv = document.createElement("div");
-    const authorDiv = document.createElement("div");
-    const pagesDiv = document.createElement("div");
-    const statusDiv = document.createElement("div");
-    const Title = document.createElement("p");
-    const Author = document.createElement("p");
-    const NoOfPages = document.createElement("p");
-    const ReadStatus = document.createElement("p");
-    const titleLabel = document.createElement("p");
-    const authorLabel = document.createElement("p");
-    const noOfPagesLabel = document.createElement("p");
-    const readStatusLabel = document.createElement("p");
+    // diaplay book on the main section in html page
+    displayBook() {
+        for (let book of library.myLibrary) {
+            this.createCard(book);
+        };
+    };
 
-    ReadStatus.setAttribute("id", "readingStatus");
-    
-    titleDiv.setAttribute("class", "card-row");
-    titleLabel.textContent = 'Title';
-    titleDiv.appendChild(titleLabel);
-    titleDiv.appendChild(Title);
+    // Display the searched book
+    displaySearchedBook(searchedInput) {
 
-    authorDiv.setAttribute("class", "card-row");
-    authorLabel.textContent = 'Author';
-    authorDiv.appendChild(authorLabel);
-    authorDiv.appendChild(Author);
-
-    pagesDiv.setAttribute("class", "card-row");
-    noOfPagesLabel.textContent = 'No. of Pages';
-    pagesDiv.appendChild(noOfPagesLabel);
-    pagesDiv.appendChild(NoOfPages);
-
-    statusDiv.setAttribute("class", "card-row");
-    readStatusLabel.textContent = 'Read Status';
-    statusDiv.appendChild(readStatusLabel);
-    statusDiv.appendChild(ReadStatus);
-
-    Title.textContent = book.title;
-    Author.textContent = book.author;
-    NoOfPages.textContent = book.noOfPages;
-    ReadStatus.textContent = book.readStatus + 	' 📚';
-
-    ReadStatus.addEventListener('click', () => {
-        book.readStatus = "Read";
-        mainDisplay.textContent = "";
-        displayBook();
-    });
-
-    const editDeleteCard = document.createElement("div");
-    editDeleteCard.setAttribute("class", "card-buttons");;
-    const deleteCard = document.createElement("button");
-    deleteCard.textContent = "Delete";
-    
-    deleteCard.addEventListener('click', () => {
-        const index = myLibrary.indexOf(book);
-        myLibrary.splice(index, 1);
-        bookCard.remove();
-    });
-
-    editDeleteCard.appendChild(deleteCard);
-
-    bookCard.appendChild(titleDiv);
-    bookCard.appendChild(authorDiv);
-    bookCard.appendChild(pagesDiv);
-    bookCard.appendChild(statusDiv);
-    bookCard.appendChild(editDeleteCard);
-    return mainDisplay.appendChild(bookCard);
-}
-
-// diaplay book on the main section in html page
-function displayBook() {
-    for (let book of myLibrary) {
-        createCard(book);
-    }
-}
-
-// Display the searched book
-function displaySearchedBook(searchedInput) {
-
-    for (const book of myLibrary) {
-        if (book.title === searchedInput) {
-            mainDisplay.textContent = '';
-            return createCard(book);
+        for (const book of library.myLibrary) {
+            if (book.title === searchedInput) {
+                mainDisplay.textContent = '';
+                return this.createCard(book);
+            }
         }
+        this.bookCard.textContent = 'No Book found.';
+        mainDisplay.appendChild(this.bookCard);
+    };
+
+};
+
+// class based constructor function for creating new book objectc
+class Book extends library {
+    constructor (title, author, noOfPages, readStatus) {
+        super();
+        this.title = title;
+        this.author = author;
+        this.noOfPages = noOfPages;
+        this.readStatus = readStatus;
     }
-    bookCard.textContent = 'No Book found.';
-    mainDisplay.appendChild(bookCard);
-}
+
+    // create a new card div for storing a book's info
+    createCard(book) {
+        const bookCard = document.createElement("div");
+        bookCard.setAttribute("class", "book-card");
+        const titleDiv = document.createElement("div");
+        const authorDiv = document.createElement("div");
+        const pagesDiv = document.createElement("div");
+        const statusDiv = document.createElement("div");
+        const Title = document.createElement("p");
+        const Author = document.createElement("p");
+        const NoOfPages = document.createElement("p");
+        const ReadStatus = document.createElement("p");
+        const titleLabel = document.createElement("p");
+        const authorLabel = document.createElement("p");
+        const noOfPagesLabel = document.createElement("p");
+        const readStatusLabel = document.createElement("p");
+    
+        ReadStatus.setAttribute("id", "readingStatus");
+        
+        titleDiv.setAttribute("class", "card-row");
+        titleLabel.textContent = 'Title';
+        titleDiv.appendChild(titleLabel);
+        titleDiv.appendChild(Title);
+    
+        authorDiv.setAttribute("class", "card-row");
+        authorLabel.textContent = 'Author';
+        authorDiv.appendChild(authorLabel);
+        authorDiv.appendChild(Author);
+    
+        pagesDiv.setAttribute("class", "card-row");
+        noOfPagesLabel.textContent = 'No. of Pages';
+        pagesDiv.appendChild(noOfPagesLabel);
+        pagesDiv.appendChild(NoOfPages);
+    
+        statusDiv.setAttribute("class", "card-row");
+        readStatusLabel.textContent = 'Read Status';
+        statusDiv.appendChild(readStatusLabel);
+        statusDiv.appendChild(ReadStatus);
+    
+        Title.textContent = book.title;
+        Author.textContent = book.author;
+        NoOfPages.textContent = book.noOfPages;
+        ReadStatus.textContent = book.readStatus + 	' 📚';
+    
+        ReadStatus.addEventListener('click', () => {
+            book.readStatus = "Read";
+            mainDisplay.textContent = "";
+            this.displayBook();
+        });
+    
+        const editDeleteCard = document.createElement("div");
+        editDeleteCard.setAttribute("class", "card-buttons");;
+        const deleteCard = document.createElement("button");
+        deleteCard.textContent = "Delete";
+        
+        deleteCard.addEventListener('click', () => {
+            const index = library.myLibrary.indexOf(book);
+            library.myLibrary.splice(index, 1);
+            bookCard.remove();
+        });
+    
+        editDeleteCard.appendChild(deleteCard);
+    
+        bookCard.appendChild(titleDiv);
+        bookCard.appendChild(authorDiv);
+        bookCard.appendChild(pagesDiv);
+        bookCard.appendChild(statusDiv);
+        bookCard.appendChild(editDeleteCard);
+        return mainDisplay.appendChild(bookCard);
+    }
+    
+};
 
 // Shows form for the user to add new book
 addBookBtn.addEventListener('click', () => {
@@ -131,28 +139,29 @@ bookForm.addEventListener('submit', (e) => {
     const noOfPages = data.get('pages');
     const readStatus = data.get('status');
 
-    addBookToLibrary(title, author, noOfPages, readStatus);
+    const book = new Book(title, author, noOfPages, readStatus);
+    book.addBookToLibrary(book);
     bookFormModal.close();
     bookForm.reset();
     mainDisplay.innerHTML = '';
-    displayBook();
-});
+    book.displayBook();
 
+    // gets the searched input and calls the display search book function 
+    searchBtn.addEventListener('click', () => {
+        const searchedInput = searchInput.value;
+        if (!searchInput.value) {
+            mainDisplay.textContent = "";
+            book.displayBook();
+        } 
+        book.displaySearchedBook(searchedInput);
+    });
 
-// gets the searched input and calls the display search book function 
-searchBtn.addEventListener('click', () => {
-    const searchedInput = searchInput.value;
-    if (!searchInput.value) {
-        mainDisplay.textContent = "";
-        displayBook();
-    } 
-    displaySearchedBook(searchedInput);
-});
+    // Option to enable searching even the ENTER key is pressed instead of the search button
+    searchInput.addEventListener('keypress', (e) => {
+        if (e.key === "Enter") {
+            e.preventDefault();
+            searchBtn.click();
+        }
+    });
 
-// Option to enable searching even the ENTER key is pressed instead of the search button
-searchInput.addEventListener('keypress', (e) => {
-    if (e.key === "Enter") {
-        e.preventDefault();
-        searchBtn.click();
-    }
 });
